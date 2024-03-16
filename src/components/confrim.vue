1c6b87c3d3f9af9e56dd9163d02ffd8ed3854bc6
dselokela023 <dselokela023@student.wethinkco.de>
2024-03-16 14:49:18 +0200

    front

---
+++ src/components/confrim.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+      <h1>Registration</h1>
+      <form @submit.prevent="register">
+        <div>
+          <label for="name">Name:</label>
+          <input type="text" id="name" v-model="registration.name" required>
+        </div>
+        <div>
+          <label for="email">Email:</label>
+          <input type="email" id="email" v-model="registration.email" required>
+        </div>
+        <div>
+          <label for="password">Password:</label>
+          <input type="password" id="password" v-model="registration.password" required>
+        </div>
+        <div>
+          <label for="idNumber">ID Number:</label>
+          <input type="text" id="idNumber" v-model="registration.idNumber" required>
+          <!-- Add validation messages here if needed -->
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        registration: {
+          name: '',
+          email: '',
+          password: '',
+          idNumber: ''
+        }
+      };
+    },
+    methods: {
+      register() {
+        // Here you can send the registration data to your backend server
+        console.log('Registration data:', this.registration);
+        // You can also perform any additional validation before submitting the form
+      }
+    }
+  };
+  </script>
+  
